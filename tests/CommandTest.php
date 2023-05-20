81b2e5f5b18a14a80cd2713d5361ccfbf28e2c7f
Alexssander Cusin <alexssander.cusin@atlastechnol.com>
2023-05-20 10:17:28 -0300

    add command show details a panic control

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -4,16 +4,25 @@
  * Test command for create a panic control
  */
 
+use Illuminate\Console\Command;
 use PanicControl\Models\PanicControl;
 
 test('show all panic control on command')->todo();
+
 test('show details a panic control on command', function () {
-    PanicControl::factory()->create([
+    $panic = PanicControl::factory()->create([
         'service' => 'test',
     ]);
 
     $this->artisan('panic-control:show', ['service' => 'test'])
-        ->assertExitCode(0);
+        ->expectsOutput("service: {$panic->service}")
+        ->expectsOutput("description: {$panic->description}")
+        ->expectsOutput("status: {$panic->status}")
+        ->assertExitCode(Command::SUCCESS);
+
+    $this->artisan('panic-control:show', ['service' => 'not-found'])
+        ->expectsOutput('Panic Control não encontrado.')
+        ->assertExitCode(Command::FAILURE);
 });
 
 test('active a panic control on command')->todo();
