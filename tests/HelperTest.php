bdac1cc91f58fe5578dcd4335ed357accf026c6a
Alexssander Cusin <alexssander.cusin@atlastechnol.com>
2023-07-07 21:20:10 -0300

    refactore: migrate CommandTest for support a Store

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -1,11 +1,16 @@
 <?php
 
+use PanicControl\Facades\PanicControl;
 use PanicControl\Models\PanicControl as PanicControlModel;
 
 test('verify status on helper', function () {
-    $panic = PanicControlModel::factory()->create(['status' => true]);
-    $this->assertTrue(getPanicControlActive($panic->name));
+    $panic = PanicControl::create(PanicControlModel::factory()->make([
+        'status' => true,
+    ])->toArray());
+    $this->assertTrue(getPanicControlActive($panic['name']));
 
-    $panic = PanicControlModel::factory()->create(['status' => false]);
-    $this->assertFalse(getPanicControlActive($panic->name));
+    $panic = PanicControl::create(PanicControlModel::factory()->make([
+        'status' => false,
+    ])->toArray());
+    $this->assertFalse(getPanicControlActive($panic['name']));
 });
