ec1c445efa72db9657dbbb624cabb91c4d9c8aa3
Alexssander Cusin <alexssander.cusin@atlastechnol.com>
2023-07-07 21:18:09 -0300

    refactore: migrate CommandTest for Store Support

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -9,23 +9,26 @@ use PanicControl\Facades\PanicControl;
 use PanicControl\Models\PanicControl as PanicControlModel;
 
 test('show all panic control on command', function () {
-    $panics = PanicControlModel::factory()->count(3)->create();
+    $panics = PanicControlModel::factory()->count(3)->make()->toArray();
+    foreach ($panics as $panic) {
+        PanicControl::create($panic);
+    }
 
     $this->artisan('panic-control:list')
-        ->expectsOutputToContain($panics[0]->name)
-        ->expectsOutputToContain($panics[1]->name)
-        ->expectsOutputToContain($panics[2]->name)
+        ->expectsOutputToContain($panics[0]['name'])
+        ->expectsOutputToContain($panics[1]['name'])
+        ->expectsOutputToContain($panics[2]['name'])
         ->assertExitCode(Command::SUCCESS);
 
 });
 
 test('show details a panic control on command', function () {
-    $panic = PanicControlModel::factory()->create([
+    $panic = PanicControl::create(PanicControlModel::factory()->make([
         'name' => 'test',
-    ]);
+    ])->toArray());
 
     $this->artisan('panic-control:show', ['name' => 'test'])
-        ->expectsOutputToContain($panic->name)
+        ->expectsOutputToContain($panic['name'])
         ->assertExitCode(Command::SUCCESS);
 
     $this->artisan('panic-control:show', ['name' => 'not-found'])
@@ -34,29 +37,29 @@ test('show details a panic control on command', function () {
 });
 
 test('active a panic control on command', function () {
-    $panic = PanicControlModel::factory()->create([
+    $panic = PanicControl::create(PanicControlModel::factory()->make([
         'status' => false,
-    ]);
+    ])->toArray());
 
-    expect(PanicControl::check($panic->name))->toBeFalse();
+    expect(PanicControl::check($panic['name']))->toBeFalse();
 
-    $this->artisan('panic-control:active', ['name' => $panic->name])
+    $this->artisan('panic-control:active', ['name' => $panic['name']])
         ->expectsOutput('Panic Control ativado com sucesso.')
         ->assertExitCode(Command::SUCCESS);
 
-    expect(PanicControl::check($panic->name))->toBeTrue();
+    expect(PanicControl::check($panic['name']))->toBeTrue();
 });
 
 test('deactive a panic control on command', function () {
-    $panic = PanicControlModel::factory()->create([
+    $panic = PanicControl::create(PanicControlModel::factory()->make([
         'status' => true,
-    ]);
+    ])->toArray());
 
-    expect(PanicControl::check($panic->name))->toBeTrue();
+    expect(PanicControl::check($panic['name']))->toBeTrue();
 
-    $this->artisan('panic-control:desactive', ['name' => $panic->name])
+    $this->artisan('panic-control:desactive', ['name' => $panic['name']])
         ->expectsOutput('Panic Control desativado com sucesso.')
         ->assertExitCode(Command::SUCCESS);
 
-    expect(PanicControl::check($panic->name))->toBeFalse();
+    expect(PanicControl::check($panic['name']))->toBeFalse();
 });
