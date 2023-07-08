517e2ddececb0c3c95a4fff823f8985cf08227c5
Alexssander Cusin <alexssander.cusin@atlastechnol.com>
2023-07-08 08:20:10 -0300

    test: add file in stores

--- tests/Datasets/Stores.php
+++ tests/Datasets/Stores.php
@@ -2,4 +2,5 @@
 
 dataset('stores', [
     ['store.database', fn () => config()->set('panic-control.default', 'database')],
+    ['store.file', fn () => config()->set('panic-control.default', 'file')],
 ]);
